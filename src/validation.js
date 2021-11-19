728733b52ca840e8e0a791ad0007ab04fe741ec9
yamngu <phuln8@gmail.com>
2021-11-19 14:30:24 +0700

    fix validation

--- src/validation.js
+++ src/validation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 //register validation
 const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(6).required(),
+    name: Joi.string().required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
   });
