faeb19b1487674d0cdc61da54aae47321bc27599
yamngu <phuln8@gmail.com>
2021-11-19 20:03:39 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
+const cors = require("cors");
 
 const AuthRoute = require("./routes/auth");
 const UserRoute = require("./routes/user");
@@ -16,6 +17,7 @@ mongoose.connect(
 );
 
 app.use(express.json());
+app.use(cors);
 
 const port = process.env.PORT || 3333;
 
